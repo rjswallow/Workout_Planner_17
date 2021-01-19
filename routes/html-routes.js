fe7451e8568c5c3ec445caead09cfc5857d19bd2
Ralds Oril <robertjswallow@gmail.com>
2021-01-18 19:23:01 -0800

    html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,7 +1,22 @@
 var router = require("express").Router()
 var path = require("path")
 
-
-
-
+    
+    app.get("/", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+  
+   
+    app.get("/exercise", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/excercise.html"));
+    });
+  
+ 
+    app.get("/stats", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/stats.html"));
+    });
+  
+  
+ 
 module.exports = router
+
